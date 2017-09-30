dbe8b8d197d1e2e1aecdbf1c6830607ae0cb9a3c
MAGWING <fsh179828@126.com>
2017-09-30 18:22:15 +0800

    Add files via upload

--- js/window.js
+++ js/window.js
@@ -10,13 +10,7 @@ titles = {
 p_size=titles.font_size-2;
 
 //function
-var win = function(title,head,type){
-if(type=='choose'){
-	choose='';
-}
-else{
-choose='';
-}
+var ios = function(title,head,type){
     width=screen.availWidth;
 	height=screen.availHeight;
 	left=width/2-134;
@@ -37,13 +31,13 @@ choose='';
 	$("html").append("<window></window>");
 	$("body").fadeTo(0,0.05);
 	choose_wid=windows.wid/2-8.5;
-		if(type=='choose'){
-	choose='<tools id="tools_ground"><tools style="font-size:'+titles.font_size+'px;"><c id="false" class="a" onclick="clos(false);" style="border-radius:0px 0px 0px 15px;background:#f2f2f2;">取消</c><a style="float:left;width:0.01px;height:45px;font-weight:bold;background-color:#b5b5b5;text-align:center;padding:0.5px;text-decoration:none;text-transform:uppercase;"></a><c id="true" class="a" onclick="clos(true);"style="border-radius:0px 0px 15px 0px;background:#f2f2f2;"><b>确定</b></c>'+choose+'</tools></tools></div>';
+		if(type=='confirm'){
+	choose='<tools id="tools_ground"><tools style="font-size:'+titles.font_size+'px;"><c id="false" class="a" onclick="clos(false);" style="border-radius:0px 0px 0px 15px;background:#f2f2f2;">取消</c><a style="float:left;width:0.01px;height:45px;font-weight:bold;background-color:#b5b5b5;text-align:center;padding:0.5px;text-decoration:none;text-transform:uppercase;"></a><c id="true" class="a" onclick="clos(true);"style="border-radius:0px 0px 15px 0px;background:#f2f2f2;"><b>确定</b></c></tools></tools></div>';
 }
 else{
-choose='<tools id="tools_ground"><tools style="font-size:'+titles.font_size+'px;"><close onclick="clos(true);"><b>确定</b></close></tools></tools></div>';
+choose='<tools id="tools_ground"><tools style="font-size:'+titles.font_size+'px;"><c id="true1" class="a" onclick="clos(true);" style="border-radius:0px 0px 15px 0px;background:#f2f2f2;text-align:center;width:'+windows.wid+'px"><b>确定</b></c></tools></tools></div>';
 }
-	$("window").html('<style>.a{float:left;width:'+choose_wid+'px;font-weight:bold;color:#007ff;text-align:center;padding:4px;text-decoration:none;text-transform:uppercase;}::-moz-selection { /* Code for Firefox */background: transparent;}::selection {background: transparent;}body{background-color:#e8e8e8;color:#e8e8e8;}#window{cursor:default;word-wrap:break-word;color:#000;position:fixed;left:'+left+'px;top:'+top_hei+'px;width:'+windows.wid+'px;background-color: #f2f2f2;border-bottom: 1px solid #b5b5b5;border-top-left-radius: 7px;border-top-right-radius: 7px;text-align:center;border-radius: 15px 15px 0px 0px;}#tools_ground{line-height:240%;word-wrap:break-word;margin-top: 1px;color: #007aff;background: #f2f2f2;display: block;position: relative;white-space: nowrap;text-overflow: ellipsis;overflow: hidden;cursor: pointer;border-bottom: 1px solid #b5b5b5;box-sizing: border-box;position:fixed;left:'+tools_ground.left+'px;width:'+windows.wid+'px;border-radius: 0px 0px 15px 15px;}</style><!--main--><div id="window"><div style="font-size:'+titles.font_size+'px"><br><B>'+title+'</B></div><p style="font-size:'+p_size+'px">'+head+'</p>'+choose);
+	$("window").html('<style>.a{float:left;width:'+choose_wid+'px;font-weight:bold;color:#007ff;text-align:center;padding:4px;text-decoration:none;text-transform:uppercase;}::-moz-selection { /* Code for Firefox */background: transparent;}::selection {background: transparent;}body{                                                                                                                                                                                                                                                                                                                                                                                    color:#e8e8e8;}#window{cursor:default;word-wrap:break-word;color:#000;position:fixed;left:'+left+'px;top:'+top_hei+'px;width:'+windows.wid+'px;background-color: #f2f2f2;border-bottom: 1px solid #b5b5b5;border-top-left-radius: 7px;border-top-right-radius: 7px;text-align:center;border-radius: 15px 15px 0px 0px;}#tools_ground{line-height:240%;word-wrap:break-word;margin-top: 1px;color: #007aff;background: #f2f2f2;display: block;position: relative;white-space: nowrap;text-overflow: ellipsis;overflow: hidden;cursor: pointer;border-bottom: 1px solid #b5b5b5;box-sizing: border-box;position:fixed;left:'+tools_ground.left+'px;width:'+windows.wid+'px;border-radius: 0px 0px 15px 15px;}</style><!--main--><div id="window"><div style="font-size:'+titles.font_size+'px"><br><B>'+title+'</B></div><p style="font-size:'+p_size+'px">'+head+'</p>'+choose);
 	
 	//将弹窗淡入
     $("#window").fadeOut(0);
@@ -63,7 +57,15 @@ choose='<tools id="tools_ground"><tools style="font-size:'+titles.font_size+'px;
     function(){
       $(this).attr("style","border-radius:0px 0px 15px 0px;background:#f2f2f2;");
   });
+  
+    	    $("#true1").hover(function(){
+	$(this).attr("style","border-radius:0px 0px 15px 0px;background:#e8e8e8;text-align:center;width:"+windows.wid+"px");
+	},
+    function(){
+      $(this).attr("style","border-radius:0px 0px 15px 0px;background:#f2f2f2;text-align:center;width:"+windows.wid+"px");
+  });
 	});
+	
 }
 
 var js = function(code){
